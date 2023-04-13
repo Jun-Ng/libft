eed4b5b4b811fdb36b99dd86b1420ff67471ed0c
Jun <junng7777@gmail.com>
2023-04-13 16:46:17 +0800

    Changed ft_strjoin pretty

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,9 +10,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s3 = 0;
 	if (!s1 && !s2)
 		return (0);
-	if (!s1 || !ft_strlen(s1))
+	if (!s1 || !*s1)
 		return (ft_strdup(s2));
-	if (!s2 || !ft_strlen(s2))
+	if (!s2 || !*s2)
 		return (ft_strdup(s1));
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
